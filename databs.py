b4116c30ef75ded794c237d4144c0f8661c3f941
zephyr2403 <ashutosh.extrimis.2403@gmail.com>
2017-12-14 15:00:58 +0530

    Added Edit Connector

--- databs.py
+++ databs.py
@@ -3,6 +3,15 @@ sql_conn = mdb.connect('localhost','root','1234567','autologin')
 cursor = sql_conn.cursor()
 cursor.execute('use autologin')
 
+def edit_entry(table,name,url,username,password,oldname):
+    #try:
+        cursor.execute('UPDATE %s SET name="%s", url="%s",username="%s",password="%s" WHERE name="%s"'%(table,name,url,username,password,oldname))
+        sql_conn.commit()
+        cursor.execute('SELECT * FROM %s'%(table))
+        return cursor.fetchall()
+    #except:
+        return 0
+
 def new_entry(table,name,url,username,password):
     try:
         cursor.execute("INSERT INTO %s VALUES ('%s','%s','%s','%s')"%(table,name,url,username,password))
@@ -11,15 +20,13 @@ def new_entry(table,name,url,username,password):
         return cursor.fetchall()
     except:
         return 0
+
 def new_user(user,passwd):#function to create and insert new user in the database
     try:
-        print 'yes'
         cursor.execute("INSERT INTO users (username,password) VALUES (%s,%s)",(user,passwd))
-        print 'yes'
-        cursor.execute("CREATE TABLE %s (name varchar(100) not null,url varchar(5000) not null,username varchar(100) not null,password varchar(100) not null)"%(user))
-        print 'yes'
+        cursor.execute("CREATE TABLE %s (name varchar(100) not null unique,url varchar(5000) not null,username varchar(100) not null,password varchar(100) not null)"%(user))
         url='https://accounts.google.com/signin/v2/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin'
-        cursor.execute("INSERT INTO %s VALUES ('Gmail','%s','InsertUsername','InsertPassword')"%(user,url))
+        cursor.execute("INSERT INTO %s VALUES ('Gmail','%s','Username.date@gmail.com','InsertPassword')"%(user,url))
         sql_conn.commit()
         return 1
     except:
--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from Tkinter import *
 import time
 import databs
+import driver
 
 def change_text(root,ilabel,text,ftext,iclr,fclr,spd): # function to change status and give update to the user what to do and what is happening
     ilabel.config(text=text,fg=iclr);
@@ -9,8 +10,19 @@ def change_text(root,ilabel,text,ftext,iclr,fclr,spd): # function to change stat
     ilabel.config(text=ftext,fg=fclr);
     root.update()
 
+def edit_connector(edit_box,ilabel,name,url,username,passwd,oldname):#old name to find the name to be fetched and changed in the database
+    global userb
+    if(not name or not url or not username or not passwd):
+        change_text(edit_box,ilabel,'Please Fill All Entries ','Enter Details','red','white',1)
+    else:
+        inserted=databs.edit_entry(userb,name,url,username,passwd,oldname)
+        if(inserted==0):
+            change_text(edit_box,ilabel,'Error Occured.Please Try Again','Enter Details','red','white',1)
+        else:
+            main_box(inserted,edit_box)
 def add_connector(addbox,ilabel,name,url,username,passwd):
     global userb
+
     if(not name or not url or not username or not passwd):
         change_text(addbox,ilabel,'Please Fill All Entries ','Enter Details','red','white',1)
     else:
@@ -19,6 +31,7 @@ def add_connector(addbox,ilabel,name,url,username,passwd):
             change_text(addbox,ilabel,'Error Occured.Please Try Again','Enter Details','red','white',1)
         else:
             main_box(inserted,addbox)
+
 def new_entry_box(details,master):
     master.destroy()
     addbox = Tk()
@@ -48,8 +61,37 @@ def new_entry_box(details,master):
     Button(addbox,text="Cancel",relief=GROOVE,font="Tahoma 12",fg="white",bg="#404042",width=8,command=lambda:main_box(details,addbox)).grid(row=6,column=1,columnspan=2,padx='10px',pady='5px')
 
 
-def edit_box(det):
-    print det
+def edit_box(details,x,master):
+    master.destroy()
+    addbox = Tk()
+    addbox.config(bg="#404042")
+    addbox.wm_title('Enter Details For New Site')
+
+    ilabel=Label(addbox,text='Enter Details',fg="#ffffff",font="Tahoma 12 bold",bg="#404042")
+    ilabel.grid(row=0,column=0,padx=(10,0),pady=(5,5),columnspan=3)
+
+    Label(addbox,text='Name',fg="#ffffff",font="Tahoma 12 bold",bg="#404042").grid(row=1,column=0,padx=(10,5),pady=(5,5))
+    name=Entry(addbox,width=34,fg="white",bg="#404042",borderwidth=3,font="Tahoma 12",insertbackground="white")
+    name.insert(0,details[x][0])
+    name.grid(row=1,column=1,padx=(5,10),pady=(5,5),columnspan=2)
+
+    Label(addbox,text='Url',fg="#ffffff",font="Tahoma 12 bold",bg="#404042").grid(row=2,column=0,padx=(10,5),pady=(5,5))
+    url=Entry(addbox,width=34,fg="white",bg="#404042",borderwidth=3,font="Tahoma 12",insertbackground="white")
+    url.insert(0,details[x][1])
+    url.grid(row=2,column=1,padx=(5,10),pady=(5,5),columnspan=2)
+
+    Label(addbox,text='Username/Email',fg="#ffffff",font="Tahoma 12 bold",bg="#404042").grid(row=3,column=0,padx=(10,5),pady=(5,5))
+    use_name=Entry(addbox,width=34,fg="white",bg="#404042",borderwidth=3,font="Tahoma 12",insertbackground="white")
+    use_name.insert(0,details[x][2])
+    use_name.grid(row=3,column=1,padx=(5,10),pady=(5,5),columnspan=2)
+
+    Label(addbox,text='Password',fg="#ffffff",font="Tahoma 12 bold",bg="#404042").grid(row=5,column=0,padx=(10,5),pady=(5,5))
+    pass_wd=Entry(addbox,width=34,show="*",fg="white",bg="#404042",borderwidth=3,font="Tahoma 12",insertbackground="white")
+    pass_wd.grid(row=5,column=1,padx=(5,10),pady=(5,5),columnspan=2)
+                                                                                                                                                                                            #details[x][0] is oldname
+    Button(addbox,text="Edit",relief=GROOVE,font="Tahoma 12",fg="white",bg="#404042",width=8,command=lambda:edit_connector(addbox,ilabel,name.get(),url.get(),use_name.get(),pass_wd.get(),details[x][0])).grid(row=6,column=0,columnspan=2,padx='10px',pady='5px')
+    Button(addbox,text="Cancel",relief=GROOVE,font="Tahoma 12",fg="white",bg="#404042",width=8,command=lambda:main_box(details,addbox)).grid(row=6,column=1,columnspan=2,padx='10px',pady='5px')
+
 def main_box(details,*args):
     try:
         args[0].destroy()
@@ -65,10 +107,11 @@ def main_box(details,*args):
         for j in range(0,4):
             if(j%2==0):
                 Label(master,text=details[i][j],fg="#ffffff",font="Tahoma 12",bg="#404042").grid(row=i+1,column=j,padx='10px',pady='5px')
-        Button(master,text="Edit",relief=GROOVE,font="Tahoma 12",fg="white",bg="#404042",width=8,command=lambda: edit_box(details[i])).grid(row=i+1,column=3,padx='10px',pady='5px')
-        Button(master,text="Use",relief=GROOVE,font="Tahoma 12",fg="white",bg="#404042",width=8).grid(row=i+1,column=4,padx='10px',pady='5px')
+        Button(master,text="Edit",relief=GROOVE,font="Tahoma 12",fg="white",bg="#404042",width=8,command=lambda x=i: edit_box(details,x,master)).grid(row=i+1,column=3,padx='10px',pady='5px')
+        Button(master,text="Use",relief=GROOVE,font="Tahoma 12",fg="white",bg="#404042",width=8,command=lambda x=i: driver.auto_login(details[x][1],details[x][2],details[x][3])).grid(row=i+1,column=4,padx='10px',pady='5px')
     Button(master,text="Add",relief=GROOVE,font="Tahoma 12",fg="white",bg="#404042",width=8,command=lambda:new_entry_box(details,master)).grid(row=i+2,column=0,columnspan=3,padx='10px',pady='5px')
     Button(master,text="Logout",relief=GROOVE,font="Tahoma 12",fg="white",bg="#404042",width=8,command=lambda:starter(master)).grid(row=i+2,column=3,columnspan=2,padx='10px',pady='5px')
+
 def toggler(lab,btn): #Toggles Gui  b/w Login and Creator
     global check,root
     if(check==0):
@@ -89,7 +132,6 @@ def connector(user,passwd):  #if check == 0 Login and if check ==1 Create User
         if(inserted==0):
             change_text(root,ilabel,"Invalid Username Or Password",ftext,"red",fclr,1)
         else:
-            print inserted
             change_text(root,ilabel,"Please wait.",'Please wait..',"#00cccc",fclr,1)
             change_text(root,ilabel,"Please wait..",'Logged In',"#00cccc",'#00cccc',1)
             userb=user
